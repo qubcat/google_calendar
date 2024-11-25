48b9c6386fb53c4967100d9096714703733f2b36
qubcat <qubcat@ya.ru>
2024-11-25 21:31:15 +0200

    add import static com.codeborne.selenide.Selenide.open; add open browser

--- src/CopyGoogleCalendar.java
+++ src/CopyGoogleCalendar.java
@@ -2,12 +2,17 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class CopyGoogleCalendar {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         // Устанавливаем настройки Selenide
         Configuration.browser = "chrome";
 
+
+        open("https://www.google.com"); /** открываем браузер Google.com*/
 
     }
 }
