88c93c655e36c82152edc87a240c3cbad0f8aa9d
qubcat <qubcat@ya.ru>
2024-11-25 21:12:59 +0200

    add imports add pom.xml

---
+++ src/CopyGoogleCalendar.java
@@ -0,0 +1,13 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+public class CopyGoogleCalendar {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Устанавливаем настройки Selenide
+        Configuration.browser = "chrome";
+
+
+    }
+}
